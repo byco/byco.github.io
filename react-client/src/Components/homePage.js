cfc984238773c8016d53936845535a194406cd00
byco <30761262+byco@users.noreply.github.com>
2019-04-17 21:06:24 -0700

    add tic tac toe game to test

--- react-client/src/Components/homePage.js
+++ react-client/src/Components/homePage.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
 
-class Game extends React.Component {
+class Square extends React.Component {
   render() {
     return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
       <div>
-        Here is a function
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
-}      
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}    
       
 class HomePage extends Component {
   render() {
