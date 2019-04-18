1db386d2758830f06bb35e3f8a5b9259fb6c5752
byco <30761262+byco@users.noreply.github.com>
2019-04-17 20:58:37 -0700

    add function for test

--- react-client/src/Components/homePage.js
+++ react-client/src/Components/homePage.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        Here is a function
+      </div>
+    );
+  }
+}      
+      
 class HomePage extends Component {
   render() {
     return (
       <div>
         Home page
+        <Game />
       </div>
     )
   }
