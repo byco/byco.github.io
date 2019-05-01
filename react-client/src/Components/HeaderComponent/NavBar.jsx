90075e04565329b46746295aceab4866de489a7c
byco <30761262+byco@users.noreply.github.com>
2019-05-01 01:07:13 -0700

    correct comment block

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -5,12 +5,12 @@ class NavBar extends Component {
   render() {
     return (
       <header>
-             /*<ul id="nav">
+        {/*<ul id="nav">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">FAQ</a></li>
                 <li><a href="#">Contact</a></li>
-              </ul>*/
+              </ul>*/}
     
       </header>
     )
