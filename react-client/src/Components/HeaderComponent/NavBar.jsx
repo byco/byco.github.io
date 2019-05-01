212387795f6263169e8523ef8077ef257bf4444b
byco <30761262+byco@users.noreply.github.com>
2019-04-30 17:20:54 -0700

    correct link to downloaded stylesheet

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import 'https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css';
+import '../../../dist/coreui.min.css';
 
 class NavBar extends Component {
   render() {
