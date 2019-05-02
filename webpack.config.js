2d81d936020d020e1fe0656ac15a75f7437b299f
byco <30761262+byco@users.noreply.github.com>
2019-05-01 18:17:19 -0700

    update to projects.jsx from index

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const DIST_DIR = path.join(__dirname, '/react-client/dist');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: `${SRC_DIR}/index.jsx`,
+	entry: `${SRC_DIR}/projects.jsx`,
 	output: {
 		path: DIST_DIR,
 		filename: 'bundle.js',
